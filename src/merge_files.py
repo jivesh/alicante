d388debd4c15ffcb9011203c56d87416e7fca9d6
Rahul0506 <rahul.gidijala@gmail.com>
2022-04-15 02:44:54 +0800

    Modify merge file

--- src/merge_files.py
+++ src/merge_files.py
@@ -3,12 +3,12 @@ import sys
 try:
     MACHINE_FILE = sys.argv[1]
 except:
-    raise Exception("Try `merge_files.py NAME_OF_VM_FILE`")
+    raise Exception("No input file given. Try `python merge_files.py NAME_OF_VM_FILE`")
 
 f1 = open("compile.js", "r")
 f2 = open(MACHINE_FILE, "r")
 
-file_out = open("machine_out.js", "w")
+file_out = open("compiled_" + MACHINE_FILE, "w")
 
 for line in f1.readlines():
     file_out.write(line)
